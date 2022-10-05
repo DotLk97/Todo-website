8c195fecb358a494b1742dd5f6f318a7e7fcb0c2
Pasindu Sandaruwan <pasindusandaruwan510@gmail.com>
2022-10-05 10:47:18 +0530

    corrected the methods in edit.handlebars

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars')
-const todos = require("./Todo")
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const todos = require("./Todo");
+var bodyParser = require("body-parser");
 
 const app = express();
 
-
 //Handlebars middleware
-app.engine('handlebars',exphbs.engine({defaultLayout:'main'}));
-app.set('view engine','handlebars');
-
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Put these statements before you define any routes.
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //Home page route
-app.get('/',(req,res)=> res.render('index',{
-  title:'Todo List',
-  todos
-}))
+app.get("/", (req, res) => {
+  return res.render("index", {
+    title: "Todo List",
+    todos,
+  });
+});
 
 //Edit page route
-app.get('/api/edit',(req,res)=> res.render('edit',{
-  title:'Todo List',
-  todos
-}))
-
-
+app.get("/api/edit", (req, res) =>
+  res.render("edit", {
+    title: "Todo List",
+    todos,
+  })
+);
 
 //todos API Routes
-app.use('/api/todos',require('./routes/api/todos'))
+app.use("/api/todos", require("./routes/api/todos"));
 
-app.use('/api/delete',require('./routes/api/delete'))
+app.use("/api/delete", require("./routes/api/delete"));
 
-app.use('/api/edit',require('./routes/api/edit'))
+app.use("/api/edit", require("./routes/api/edit"));
 
-app.use('/api/complete',require('./routes/api/complete'))
+app.use("/api/complete", require("./routes/api/complete"));
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
+const PORT = process.env.PORT || 11000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
